0380a51e23b91c5e3c4394b85fd2ca5406f63e04
davnav <naveendavisv@gmail.com>
2018-07-11 23:00:35 -0700

    Add files via upload

--- main.rs
+++ main.rs
@@ -34,49 +34,69 @@ fn get_window_size()-> std::io::Result<(u16,u16)>{
 }
 	  
 
-fn editor_drawrows(){
+fn editor_drawrows(r:u16){
    
    let mut i = 0;
-   for i in 0..24{
+   for i in 0..r{
 	std::io::stdout().write(b"~\r\n");
    }
 
     std::io::stdout().write(b"\x1b[H");
 }
 
-			 	
+fn editor_read_key()-> [u8;1]{
+		
+        let mut reader = std::io::stdin();
+	let mut buffer = [0;1];
+    	reader.read_exact(&mut buffer).unwrap();
+	return buffer;
+
+}
+	
+	
+fn editor_process_key(){
+     let mut c = editor_read_key();
+     while c[0] != 113 {
+	c = editor_read_key();
+	println!("{:?}\r",c);
+     }
+    
+    editor_exit();     
+}
+
+fn init_screen(termios:&termios::Termios){
+     
+    let mut new_termios = termios.clone();
+    new_termios.c_lflag &= !(ICANON | ECHO );
+    new_termios.c_oflag &= !(OPOST);	
+//    let mut termios_stdout = Termios::from(stdout).unwrap();
+    tcsetattr(0,TCSANOW, &mut new_termios).unwrap();
+    
+    editor_refreshscreen();
+    let screen_size = get_window_size();	
+    match screen_size{
+	Ok((r,c)) => editor_drawrows(r),
+	Err(e) =>  println!("{}",e),
+     
+    };
+
+}
+
+fn editor_exit(){
+					 	
+	std::io::stdout().write(b"\x1b[H");
+	std::io::stdout().write(b"\x1b[2J");
+}
+
 
 fn main() {
 
     let stdin = 0;
     let stdout = 0;
-    
     let mut termios = Termios::from_fd(stdin).unwrap();	
-    let mut new_termios = termios.clone();
-    new_termios.c_lflag &= !(ICANON | ECHO );
-    new_termios.c_oflag &= !(OPOST);	
-//    let mut termios_stdout = Termios::from(stdout).unwrap();
-    editor_refreshscreen();
-    let screen_size = get_window_size();	
-    match screen_size{
-	Ok((r,c)) => println!("row:{},column:{}",r,c),
-	Err(e) =>  println!("{}",e),
-    };    
-    editor_drawrows(); 
-    tcsetattr(stdin,TCSANOW, &mut new_termios).unwrap();
-    let mut reader = std::io::stdin();
-    let mut buffer = [0;1];
-//    println!("Hit a key");
-   
-    while buffer[0] != 113{
-	
-    	reader.read_exact(&mut buffer).unwrap();
-	println!("{:?}\r",buffer);
-    } 	
-
-	 
+    init_screen(&termios);	
+    editor_process_key();
+    editor_exit();
     tcsetattr(stdin,TCSANOW, &mut termios).unwrap();
-    
-   
 }
 
